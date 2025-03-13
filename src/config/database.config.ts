c30ac8cafa8daf37bc8f179493ce3fc8e7b91838
KHSR MACBOOK <MAC@KHSR-MacBook-Pro-M1.local>
2025-03-13 20:58:28 +0000

    rajout du service hello word

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -5,7 +5,7 @@ export const databaseConfig: TypeOrmModuleOptions = {
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: 'password',
+  password: 'azerty1234',
   database: 'auth_jwt_db',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
